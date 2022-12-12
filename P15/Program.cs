c45b8e5debbb08bd7a9602ae0cf1ca89626b00ba
MikhaiLDouG <miixasi2014@yandex.ru>
2022-12-12 22:02:58 +0300

    Task15

---
+++ P15/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace P15
+{
+    class Program
+    {
+        static Random rnd = new Random();
+        static void Print(object i)
+        {
+
+            Console.WriteLine(i.ToString());
+            Thread.Sleep(rnd.Next(1000,3000));
+            Console.WriteLine($"Task №{Task.CurrentId} закончил работу");
+        }
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Task.Factory.StartNew(Print, "Привет");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
